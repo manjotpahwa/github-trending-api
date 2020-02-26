da1f35d6cc7657f0ef0f5f7fc48b653ccc87510d
Manjot <manjotpahwa@gmail.com>
2020-02-26 20:59:15 +0530

    Adding base setup.py file

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+with open('README.md') as readme_file:
+    README = readme_file.read()
+
+setup_args = dict(
+    name='github_trending',
+    version='0.1',
+    description='Pypi package to access Github trending in Python',
+    long_description_content_type="text/markdown",
+    long_description=README,
+    license='MIT',
+    packages=find_packages(),
+    author='Manjot Pahwa',
+    author_email='manjot.pahwa@gmail.com',
+    keywords=['Github', 'Github Trending'],
+    url='https://github.com/manjotpahwa/github-trending-api',
+    download_url='https://pypi.org/project/github-trending-api/'
+)
+
+install_requires = [
+    'aiohttp'
+]
+
+if __name__ == '__main__':
+    setup(**setup_args, install_requires=install_requires)
